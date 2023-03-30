a79d108e435ad8450dc619b79d8bce4dc4f878a6
Григорий Панов <g89539686522@yandex.ru>
2023-03-30 16:35:34 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Region
+    {
+        public static string Names (int number)
+        {
+            if (number==01||number==701||number==901)
+            {
+                return "Республика Адыгея";
+            }
+            return "номер не найден в базе";
+        }
+    }
+}
